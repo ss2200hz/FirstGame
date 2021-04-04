06feeffccd82a00bd6f818ce16b5ca7150f1d080
ss2200hz <ss2200hz@163.com>
2021-04-04 17:27:20 +0800

    first commit asset folder

---
+++ Scripts/Camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    GameObject stone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stone = GameObject.FindGameObjectsWithTag("stone")[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = stone.transform.position.x;
+        float y = stone.transform.position.y;
+        float z = stone.transform.position.z - 1.5f;
+        transform.position = new Vector3(x, y, z);
+    }
+}
---
+++ Scripts/Stone.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Stone : MonoBehaviour
+{
+    private bool canBeForce;
+    private bool isOnhill;
+    public int forceNum;
+    public double hight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.canBeForce = true;
+        this.isOnhill = false;
+        this.hight = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (canBeForce)
+        {
+            //添加推力，区分是否在坡上
+            Vector3 pushForce = new Vector3(0, 0, forceNum);
+            if (this.isOnhill)
+            {
+                pushForce = new Vector3(0, forceNum, forceNum);//依照45度坡推动
+            }
+            //处理交互事件
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log(pushForce);
+                //推动石头
+                GetComponent<Rigidbody>().AddForce(pushForce, ForceMode.Impulse);
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //到达底部后切换是否在坡上状态
+        if (other.CompareTag("hilldown"))
+        {
+            isOnhill = !isOnhill;
+        }
+        Debug.Log(other.name+" "+isOnhill);
+    }
+}
